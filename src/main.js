8c4844aebc7eb93a05d78facce8f771a55f476f8
nitishchoudhari <nitishchoudhari2904@gmail.com>
2024-01-28 22:57:56 +0530

    glider-js

--- src/main.js
+++ src/main.js
@@ -4,3 +4,18 @@ const navList = document.getElementById('nav-list');
 hamBurger.addEventListener('click', () => {
     navList.classList.toggle('show');
 });
+
+window.addEventListener('load', function () {
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        draggable: true,
+        dots: '.dots',
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        }
+    });
+})
+
+
